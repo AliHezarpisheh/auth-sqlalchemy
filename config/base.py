236e685ec6e582a8ab8acf525669fde36d5f31c8
alihezarpisheh <alihezarpisheh@outlook.com>
2024-04-24 17:22:48 +0330

    ✨ Feature(config/database): Add new database configs and utilities
    - Implement the `Base` class and `metadata` configurations to `env.py` in `alembic/`
    - Add new mixins classes
    - Add a `PEPPER` due to more user password security

--- config/base.py
+++ config/base.py
@@ -1,5 +1,9 @@
 """Module for defining base configurations."""
 
+import secrets
+
 from .database.base import DatabaseConnection
 
 db = DatabaseConnection()
+
+PEPPER = secrets.token_urlsafe(40).encode()
