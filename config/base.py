4ed6ef5ed89448aafc04ddc97bbe268b01d5960c
alihezarpisheh <alihezarpisheh@outlook.com>
2024-04-25 13:06:38 +0330

    🔥 Refactor(config): Remove `PEPPER` from `base.py`

--- config/base.py
+++ config/base.py
@@ -1,9 +1,5 @@
 """Module for defining base configurations."""
 
-import secrets
-
 from .database.base import DatabaseConnection
 
 db = DatabaseConnection()
-
-PEPPER = secrets.token_urlsafe(40).encode()
